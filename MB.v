76dc6983c9f5443f41149fff6ef2711d2d166505
adpitcock <51276332+adpitcock@users.noreply.github.com>
2020-02-13 18:18:33 -0800

    Update MB.v

--- MB.v
+++ MB.v
@@ -1,16 +1,20 @@
+//Andrew Pitcock
+//CpE 166
+//Lab 2 part 2
+//4 bit shift register
 module MB(db,shb,ld,clr,clk,qb);
 input [3:0] db;
 input shb, ld, clr,clk;
 output [3:0] qb;
 reg [3:0] qb;
 
-always@(posedge clk or posedge reset)
+always@(posedge clk or posedge clr)
 begin
-	if(reset)
+	if(clr)
 		qb <= 4'b0000;
 	else if (ld)
 		qb <= db;
 	else
-		qb <= {1'b0, qb[3:1]);
+		qb <= {1'b0, qb[3:1]};
 end
 endmodule
