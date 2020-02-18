f6bba951d6225b8135b4560943687a427c31c011
adpitcock <51276332+adpitcock@users.noreply.github.com>
2020-02-17 16:03:45 -0800

    Update MULT.v

--- MULT.v
+++ MULT.v
@@ -16,7 +16,7 @@ assign ground = 4b'0000;
 MB f1(.db(db), .shb(shb), .ld(ld), .clr(clr), .clk(clk), .qb(mb_out[0]));
 MA f2(.clr(clr), .ld(ld), .clk(clk), .d1(ma_out), .da(da));
 MUXB f3(.d0(ground), .d1(ma_out), .s(mb_out[0]), .y(muxb_out));
-adder f4(.a(p[7:4]), .b(muxb_out), .cout(cout), .sum(add_out));
+adder f4(.a(p[6:3]), .b(muxb_out), .cout(cout), .sum(add_out));
 prod f5(.shp(shp), .ldp(ldp), .clr(clr), .clk(clk), .cin(cout), .sum(add_out), .p(p));
 
 endmodule
